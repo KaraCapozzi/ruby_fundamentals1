ad77de3197ce22380e819ce8403949e5b97b090f
Kara Capozzi <karacapozzi@Karas-MacBook.local>
2017-08-08 16:17:38 -0400

    add exercise 2

---
+++ exercise2.rb
@@ -0,0 +1,31 @@
+puts 55 * 0.15
+puts "string" + "25
+
+45628 * 7839
+
+#{45628 * 7839}
+
+(10 < 20 && 30 < 20) || !(10 == 11)
+
+my_variable = 'my_variable now contains this string'
+
+name = "Sandra"
+greeting = "Hello #{name}! It's good to see you again."
+mission = "Your mission, should you choose to accept it..."
+
+puts "#{greeting} #{mission}"
+
+my_number = 12
+my_number > 10
+# true
+
+my_number < 10
+# false
+
+your_number = 1
+
+my_number == your_number
+# false
+
+my_number != you_number
+# true
